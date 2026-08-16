6abd8e75227b8b063d9f67781f12b1eff381b9ba
kepty19 <kepty.19@gmail.com>
2026-08-16 20:54:26 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -981,7 +981,7 @@ export default function App() {
                   【学習量の目安】
                 </h5>
                 <ul className="text-[13px] sm:text-[15.5px] font-bold text-neutral-700 space-y-2.5 pl-4 sm:pl-8 list-disc">
-                  <li>毎日2時間の学習時間を推奨しております。</li>
+                  <li>毎日2時間の学習時間を推奨してます。</li>
                   <li>移動や治療、ストレッチの時間などを有効活用できます。</li>
                 </ul>
               </div>
